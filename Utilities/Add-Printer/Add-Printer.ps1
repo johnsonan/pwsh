462cb13965c2bcb5cd25fd53e077b4df3ab5fb87
johnsonan <30813966+johnsonan@users.noreply.github.com>
2021-08-17 17:58:33 -0400

    Delete old printer function, rename new.

--- Utilities/Add-Printer/Add-Printer.ps1
+++ Utilities/Add-Printer/Add-Printer.ps1
@@ -1,110 +1,176 @@
-﻿# This script will take the printer name as a parameter and intall it for all users.
-Param(
-  [Parameter(Mandatory=$True)]
-    [String]
-    $printServer,
+Function Add-LocalPrinter {
 
- [Parameter(Mandatory=$True)]
-    [String]
-    $printerName
-)
+    Param(
 
+        [Parameter(
+            Mandatory=$true
+        )]
+        [ValidateSet("\\org-canon", "\\org-hp", "\\org-others")]
+        [String]
+        $PrintServer,
+      
+        [Parameter(
+           Mandatory=$true
+        )]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $PrinterName,
 
-#The printer name will be passed in as a value...
-#$printServer = "\\org-others"
+        [Parameter(
+            Mandatory = $false
+        )]
+        [switch]
+        $Force
+      
+    )
+    
+    Begin {
+        
+        # Helper function for testing driver installation
+        Import-Module "$PSScriptRoot\Get-LocalDriver.ps1"
 
-write-host "Your selected printer is $printerName"
-$pathToSMB = "\\some\folder\PrinterDrivers\"
-
-
-#Create Lists for Printers...
-#$printServers = @("\\org-canon", "\\org-others", "\\org-hp")
-
-
-#pipe each printer server to the foreach loop 
-#$allPrinters = $printServers | %{ Get-Printer -ComputerName $_ }
-$allPrinters = Get-Printer -ComputerName $printServer
-
-#Get the printers IP, Driver, Print Server
-foreach($name in $allPrinters){ 
-if($printerName -Match $name.'Name'){
-$server = "\\"+$name.'ComputerName'
-$ip=$name.'PortName'
-$printerDriverName = $name.'DriverName'
-write-host "The IP for"$printerName" is "$ip
-write-host "The Driver for "$printerName" is "$printerDriverName
-write-host "Print Server: "$server
-    }
-}
-
-#Get Printers INF
-$infPath = Get-PrinterDriver -ComputerName $server -Name $printerDriverName | Select 'InfPath' 
-$infPath = $infPath.'InfPath'
-
-#If INF path returns an array get the first item... Hopefully the amd64 driver
-if($infPath.Count -gt 1){
-$infPath = $($infPath[0]).ToString()
-Write-Host $infPath}
-
-
-#Copy the driver to C:\Windows\System32\DriverStore\FileRepository\  -Doesn't work
-#So i'm going to copy the driver to C:\Temp\<Driver Folder>
-#If driver install from the print server fails, the script will copy the drivers to a temp folder for install
-
-$folderName = $infPath -replace ".*FileRepository\\" -replace "\\.*"
-$smbPathToDriver = $pathToSMB+$folderName
-#$Target = "C:\temp\"+$folderName
-#$Target = "C:\Windows\System32\DriverStore\FileRepository\"+$folderName
-copy-item -path $smbPathToDriver $Target -recurse -Force
-write-host "Driver Location is $infPath"
-$inf = $infPath -replace '.*\\'
-$tempInfPath = $Target+"\"+$inf
-
-#SMB INF Path:  Use this first. If the driver install fails use copy drivers from org-image
-$smbInfPath = Get-ChildItem -Path $smbPathToDriver -Filter "*.inf" | Select -ExpandProperty FullName
-
-if($smbInfPath -gt 1) {
-
-    $smbInfPath = $smbInfPath[0]
-
-}
-
-$Source = $smbPathToDriver  
-#(Get-Acl $Source).Access #Verify $Source Access
-#(Get-Acl $Target).Access #Verify $Target Access
-
-
-#Add the driver...
-#pnputil.exe -a "$infPath"
-#pnputil.exe -a "$smbInfPath"
-
-
-$pnpOutput = pnputil -a "$smbInfPath" | Select-String "Published name" 
-$null = $pnpOutput -match "Published name :\s*(?<name>.*\.inf)"
-$driverINF = Get-ChildItem -Path C:\Windows\INF\$($matches.Name)
-Add-PrinterDriver -Name $PrinterDriverName -InfPath $driverINF.FullName
-
-
-#Add-PrinterDriver; portName and ip are the same. I just kept different names for syntax clarity..
-$portName = $ip
-$portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
-
-#If port doesn't exist, add port...
-if (-not $portExists) {
-  Add-PrinterPort -name $portName -PrinterHostAddress $ip
-  write-host "Added Printer Port $portName"
-}
-
-
-$printDriverExists = Get-PrinterDriver -name $printerDriverName -ErrorAction SilentlyContinue
-write-host $printerDriverExists
-
-    #If driver exists ADD PRINTER!
-    if ($printDriverExists) {
-        Add-Printer -Name $printerName -PortName $portName -DriverName $printerDriverName
-    }else{
-        Write-Warning "Printer Driver not installed"
     }
 
+    Process {
 
-   
+        # Initialize remote print queue details
+        try {
+
+            $RemotePrinter = Get-Printer -ComputerName $PrintServer -Name $PrinterName -ErrorAction Stop
+            Write-Verbose "Getting printer info from $PrintServer\$PrinterName"
+
+        } catch {
+
+            throw "Unable to find network printer at: $PrintServer\$PrinterName"
+
+        }
+
+        $RemoteDriver  = Get-PrinterDriver -ComputerName $PrintServer -Name $RemotePrinter.DriverName | Select-Object -First 1
+        $RemoteInfPath = $RemoteDriver.InfPath | Select-Object -First 1
+        $DriverFolder  = $RemoteInfPath.Split("\")[-2]
+
+        # Test driver for existing installation
+        $LocalDriver = Get-LocalDriver -FolderName $DriverFolder -RemoteDriverName $RemoteDriver.Name
+
+        if ($LocalDriver -and !$Force) {
+
+            # Use existing inf file
+            $LocalInf = $LocalDriver.InfPath
+
+        } else {
+
+            # Proceed with installation
+            $Environment   = ("W32X86", "x64")[$RemoteInfPath -match "amd64"]
+            $RemoteCab     = "$PrintServer\print$\$Environment\PCC\$DriverFolder.cab"
+    
+            # Stage temp directory for driver files
+            $NewGuid = "{$((New-Guid).Guid.ToUpper())}"
+            $TempDir =  New-Item "C:\Windows\Temp\$NewGuid" -ItemType Directory
+    
+            # Expand Cab file
+            $ExpandOutput = &expand.exe "$RemoteCab" "$TempDir" -F:*
+
+            if ($ExpandOutput[-2] -notmatch "Complete") {
+    
+                throw "Error expanding $RemoteCab to $TempDir."
+    
+            } else {
+    
+                Write-Verbose "Successfully expanded $($ExpandOutput[-1].Split(" ")[0]) files from $RemoteCab to $TempDir."
+    
+            }
+    
+            # Get local Inf path and install it
+            $CabInfPath = (Get-ChildItem -Path $TempDir -Filter "*.inf").FullName
+    
+            # PnPUtil to stage remote print driver
+            $RawOutput = pnputil -a "$CabInfPath" #| Select-String "Published name"
+    
+            if ($RawOutput[-2][-1] -eq 0) {
+    
+                # If successful, the index referenced above will equal 1
+                throw "Processing pnputil on inf failed at $CabInfPath."
+    
+            } else {
+    
+                # Pull out published inf name and grab its path
+                try {
+    
+                    $PublishedName = $RawOutput.Where({ $_ -match "inf" })[-1].Split(" ")[-1]
+                    $LocalInf = (Get-ChildItem -Path "C:\Windows\INF\$PublishedName" -ea Stop).FullName
+    
+                } catch {
+    
+                    throw "Unable to find $PublishedName at C:\Windows\INF."
+    
+                }
+                
+            }
+
+        }
+
+        # Check for existing printer structures
+        $PrinterDriver = Get-PrinterDriver -Name $RemoteDriver.Name -ea SilentlyContinue
+        $PrinterPort   = Get-PrinterPort -Name $RemotePrinter.PortName -ea SilentlyContinue
+        $Printer       = Get-Printer -Name $RemotePrinter.Name -ea SilentlyContinue
+        
+        # Add printer structures if necessary
+        if (!$Force) {
+
+            if (!$PrinterDriver) {
+
+                Add-PrinterDriver -Name $RemotePrinter.DriverName -InfPath $LocalInf
+
+            } else {
+
+                Write-Verbose "Using existing printer driver."
+
+            }
+
+            if (!$PrinterPort) {
+
+                Add-PrinterPort -Name $RemotePrinter.PortName -PrinterHostAddress $RemotePrinter.PortName
+
+            } else {
+
+                Write-Verbose "Using existing printer port."
+
+            }
+
+            if (!$Printer) {
+
+                Add-Printer -Name $RemotePrinter.Name -PortName $RemotePrinter.PortName -DriverName $RemotePrinter.DriverName
+
+            } else {
+
+               throw "Printer with the name $($RemotePrinter.Name) already exists. Exiting."
+
+            }
+
+        } else {
+
+            try {
+
+                Remove-Printer -Name $RemotePrinter.Name -ea SilentlyContinue
+                Remove-PrinterPort -Name $RemotePrinter.PortName -ea SilentlyContinue
+                Remove-PrinterDriver -Name $RemotePrinter.DriverName -ea SilentlyContinue
+
+            } catch {
+
+                Write-Verbose "$PSItem"
+
+            } finally {
+
+                Add-PrinterDriver -Name $RemoteDriver.Name -InfPath $LocalInf
+                Add-PrinterPort -Name $RemotePrinter.PortName -PrinterHostAddress $RemotePrinter.PortName
+                Add-Printer -Name $RemotePrinter.Name -PortName $RemotePrinter.PortName -DriverName $RemotePrinter.DriverName
+
+            }
+
+        }
+
+    }
+
+    End { }
+
+}
